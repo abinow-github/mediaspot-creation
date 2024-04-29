b5d29568a6ad7f3a42f0f5c68ada9c0dda5cceb8
abinow <abinow.mail@gmail.com>
2024-04-29 10:55:58 +0530

    i need to add google login section thats why i commit ~ add google login

--- assets/js/product.js
+++ assets/js/product.js
@@ -5,20 +5,23 @@ $(function() {
     
     // Update href attribute on click
     $('.product-thumbnails img').on('click', function() {
+      // Update main image
       $image.css('background-image', 'url(' + $(this).attr("src") + ')');
-      $anchor.attr('href', $(this).attr("src"));
       currImage = "";
+      
+      // Remove 'hide' class from corresponding anchor tag and add to others
+      var clickedImgClass = $(this).data('img'); // Get the data-img class
+      $anchor.removeClass('hide'); // Remove 'hide' from all anchor tags
+      $anchor.not('.' + clickedImgClass).addClass('hide'); // Add 'hide' to all except clicked one
     });
   
     // Update href attribute on hover
     $('.product-thumbnails img').hover(function() {
       currImage = $image.css('background-image');
       $image.css('background-image', 'url(' + $(this).attr("src") + ')');
-      $anchor.attr('href', $(this).attr("src"));
     }, function() {
       if (currImage !== "") {
         $image.css('background-image', currImage);
-        $anchor.attr('href', $image.css('background-image'));
       }
     });
   
@@ -31,7 +34,7 @@ $(function() {
         $(this).css('transform', 'scale(1.0)');
       })
       .on('mousemove', function(e){
-          $(this).css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
-       });
+        $(this).css({'transform-origin': ((e.pageX - $(this).offset().left) / $(this).width()) * 100 + '% ' + ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +'%'});
+      });
   });
   
